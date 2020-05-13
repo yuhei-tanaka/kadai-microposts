075ff64b3183e3cf6fe650b560632acc2c0bd234
yuhei.tanaka <everythingok.9292@gmail.com>
2020-05-13 05:13:41 +0000

    likes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
+  
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -35,6 +36,12 @@ class UsersController < ApplicationController
   def followers
     @user = User.find(params[:id])
     @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+  
+  def likes
+    @user = User.find(params[:id])
+    @likes = Favorite.where(user_id:@user.id).page(params[:page])
     counts(@user)
   end
   
